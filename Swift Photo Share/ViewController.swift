5b685ba6f1bfcacb2eb0b901b342a940257745ca
empireyc <yuchuanchina@hotmail.com>
2015-03-14 20:55:32 -0400

    Initial Commit

---
+++ Swift Photo Share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Photo Share
+//
+//  Created by Chuan Yu on 3/14/15.
+//  Copyright (c) 2015 Chuan Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
